b383e1d90226eaa8d4baf16de17450ddbf2d9f5d
Tanner Watson <tannerwatson@gmail.com>
2022-09-12 10:20:50 -0400

    bump tls to 3.3.0

--- caf_solution/main.tf
+++ caf_solution/main.tf
@@ -16,7 +16,7 @@ terraform {
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 3.1.0"
+      version = "~> 3.3.0"
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
